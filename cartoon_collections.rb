5aa8725ea70747b8f958bf8ebd6dd7568bf54363
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-01 21:44:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves (array)
 end
 
 def summon_captain_planet(array)
-  "#{call[0].upcase + call[1..-1]}!"
+  array.collect { |x| x + "!" }
 end
 
 def long_planeteer_calls# code an argument here
