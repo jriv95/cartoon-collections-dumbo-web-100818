d5c53c10bff7561546ee66e20f9b56b001e8cf7c
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-01 21:57:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    cheese_types.inclue?(cheese)
+    cheese_types.include?(cheese)
   end
 end
 
